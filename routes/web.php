fcab649ea5ec56583b4fc8a5eefac9ca0f085577
ahmadrmdhann <ahmadramadhansqorpio04@gmail.com>
2025-02-17 10:24:15 +0700

    Materi Routing

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,35 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function(){
+    return 'Hello World';
+});
+
+Route::get('/world', function () {
+    return 'World';
+});
+
+Route::get('/', function () {
+    return 'Selamat Datang';
+});
+
+Route::get('/about', function () {
+    return 'Halo nama saya Ahmad Ramadhan Baiakbar, NIM saya 2341720085';
+});
+
+Route::get('user/{name}', function ($name) {
+    return 'Nama saya ' . $name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($postID, $commentID) {
+    return 'Pos ke-'.$postID." Komentar ke-".$commentID;
+});
+
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman artikel dengan ID '.$id;
+});
+
+Route::get('/user/{name?}', function ($name='John') {
+    return 'Nama saya '.$name;
+});
